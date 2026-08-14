5814d706bc700678db425c5bc6c986c5063ab7ee
Lexapskov <lexapskov@gmail.com>
2026-08-14 21:05:38 +0300

    Config backup: 2026-08-14 21:05:38

--- myoverridesbackup.sh
+++ myoverridesbackup.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 export PATH=/opt/bin:$PATH
 cd /mnt/UDISK/printer_data/config
-git add overrides.cfg printer.cfg gcode_macro.cfg box.cfg myoverridesbackup.sh README.md
+git add overrides.cfg printer.cfg gcode_macro.cfg box.cfg myoverridesbackup.sh README.md StartGcode.txt
 if git diff --cached --quiet; then
     echo "No changes to commit"
     exit 0
